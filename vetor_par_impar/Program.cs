09d2842d99a30cffe7360053839d1de4bb45d5f6
Gustavo <guvono4@gmail.com>
2024-04-22 14:20:20 -0300

    Add project files.

---
+++ vetor_par_impar/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] numeros = new int[10];
+
+//1 - leia 10 numeros inteiros e imprima os seguintes itens:
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Informe um valor para a posição {i} do vetor: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+//a) somente os valores pares;
+Console.WriteLine("Mostrando apenas os valores PARES:");
+for (int i = 1; i < 10; i += 2)
+{
+    Console.Write(numeros[i] + " ");
+}
+
+//b) somente os valores impares;
+Console.WriteLine("\nMostrando apenas os valores IMPARES:");
+for (int i = 0; i < 10; i += 2)
+{
+    Console.Write(numeros[i] + " ");
+}
+
+//c) somente a primeira e a ultima posição;
+Console.Write("\nMostrando apenas o primeiro e o ultimo valor do vetor:");
+for (int i = 0; i < 10; i++)
+{
+    if (i == 0) Console.Write(numeros[i] + " ");
+    if (i == 9) Console.Write(numeros[i] + " ");
+}
